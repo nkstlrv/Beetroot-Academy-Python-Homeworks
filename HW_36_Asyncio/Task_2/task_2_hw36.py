7126c097e9364833c3f514ea1755486545e91ef2
Nikita Stoliarov <stlrvnk@gmail.com>
2023-03-24 18:23:19 +0300

    task 2 done

---
+++ HW_36_Asyncio/Task_2/task_2_hw36.py
@@ -0,0 +1,52 @@
+import asyncio
+import aiohttp
+import aiofiles
+import json
+import time
+
+topics = ['asyncio',
+          'coroutine',
+          'global interpreter lock',
+          'algorithms and data structures',
+          'event loop',
+          'json',
+          'django']
+
+result_dict = {}
+
+
+async def get_comments(topic):
+    url = f'https://api.pushshift.io/reddit/comment/search/?q={topic}'
+
+    topic_dict = {}
+
+    async with aiohttp.ClientSession() as session:
+        reddit_resp = await session.get(url, ssl=False)
+        data = await reddit_resp.json()
+
+        for num in range(5):
+            topic_dict[data['data'][num]['author']] = {
+                'comment': data['data'][num]['body'],
+                'time': data['data'][num]['utc_datetime_str']}
+
+        result_dict[topic] = topic_dict
+
+
+async def main():
+    start = time.time()
+
+    for t in topics:
+        task = asyncio.create_task(get_comments(t))
+        await task
+
+    end = time.time()
+
+    async with aiofiles.open('async_reddit.json', 'w') as asj:
+        await asj.write(json.dumps(result_dict))
+
+    print(f'Execution time {round((end - start), 3)}')
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
