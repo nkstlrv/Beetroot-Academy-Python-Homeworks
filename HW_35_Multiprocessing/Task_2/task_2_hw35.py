973387798065b68c6f89b935cfde95fc172829c3
Nikita Stoliarov <stlrvnk@gmail.com>
2023-03-25 19:05:17 +0300

    HW35 completed

--- HW_35_Multiprocessing/Task_2/task_2_hw35.py
+++ HW_35_Multiprocessing/Task_2/task_2_hw35.py
@@ -4,6 +4,7 @@ import requests
 import json
 import concurrent.futures
 import multiprocessing
+import functools
 
 topics = ['github',
           'planes',
@@ -15,16 +16,16 @@ topics = ['github',
 
 
 def timer_decorator(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         start = time.time()
-        res = func()
+        res = func(*args, **kwargs)
         end = time.time()
         print(f"Execution time --> {round((end-start), 3)} seconds")
         return res
     return wrapper
 
 
-def get_comments(topic):
+def get_comments(topic, c_dict):
     """This function returns the most recent comments for the given topic"""
 
     URL = f'https://api.pushshift.io/reddit/comment/search/?q={topic}'
@@ -37,25 +38,25 @@ def get_comments(topic):
             'comment': reddit_r.json()['data'][num]['body'],
             'time': reddit_r.json()['data'][num]['utc_datetime_str']}
 
-    comments_dict[f'{topic}'] = topic_dict
+    c_dict[f'{topic}'] = topic_dict
 
 
 @timer_decorator
-def process_func():
+def process_func(c_dict):
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        executor.map(get_comments, topics)
-
-    # with open('reddit.json', 'w') as rj:
-    #     json.dump(dict_copy, rj)
+        executor.map(functools.partial(get_comments, c_dict=comments_dict), topics)
 
 
 if __name__ == "__main__":
 
-    manager = multiprocessing.Manager()
-    # Shared dict
-    comments_dict = manager.dict()
+    with multiprocessing.Manager() as manager:
+        # Shared dict
+        comments_dict = manager.dict()
 
-    dict_copy = dict(comments_dict)
+        process_func(comments_dict)
+        dict_copy = dict(comments_dict)
 
-    process_func()
+    with open('reddit.json', 'w') as rj:
+        json.dump(dict_copy, rj)
+
 
