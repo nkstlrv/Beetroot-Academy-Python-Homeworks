b327a95fada1c6f8d8a1e195958e2dfa5ce7cde5
Nikita Stoliarov <stlrvnk@gmail.com>
2023-04-16 22:36:28 +0300

    login register

--- Django/BeetProject/BeetProject/urls.py
+++ Django/BeetProject/BeetProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("notes.urls")),
+    path("users/", include("django.contrib.auth.urls")),
+    path("users/", include("users.urls")),
 ]
