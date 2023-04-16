a10b62568d7e434aa79d6caa635c673254c75120
Nikita Stoliarov <stlrvnk@gmail.com>
2023-04-16 23:34:14 +0300

    Registration Form

--- Django/BeetProject/users/views.py
+++ Django/BeetProject/users/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from users.forms import RegisterForm
 
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterForm
     template_name = "registration/register.html"
     success_url = reverse_lazy('login')
 
