01468a3e02c082b88d9a4118c9f7801026317372
Nikita Stoliarov <stlrvnk@gmail.com>
2023-04-17 18:19:21 +0300

    no pk views tested

--- Django/BeetProject/notes/urls.py
+++ Django/BeetProject/notes/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("new-note/", views.NewNoteView.as_view(), name='new-note'),
     path("note/<int:pk>/", views.DetailedNoteView.as_view(), name='detailed-note'),
     path("edit/<int:pk>/", views.EditNoteView.as_view(), name='edit-note'),
-    path("delete/<int:pk>/", views.DeleteNoteView.as_view(), name='delete-note'),
+    path("delete/<int:pk>/", views.DeleteNoteView.as_view(), name='edit-note'),
 ]
