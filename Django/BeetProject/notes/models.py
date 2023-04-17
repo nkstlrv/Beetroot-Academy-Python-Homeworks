9f12e7d161ef1594d2a33234c22f940c7b5e6a33
Nikita Stoliarov <stlrvnk@gmail.com>
2023-04-17 17:28:22 +0300

    ckeditor installed, migration

--- Django/BeetProject/notes/models.py
+++ Django/BeetProject/notes/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse, reverse_lazy
+from ckeditor.fields import RichTextField
 
 
 class Note(models.Model):
     title = models.CharField(max_length=255, blank=False, null=False)
-    body = models.TextField(max_length=5000, blank=False, null=False)
+    body = RichTextField(blank=False, null=False)
+    # body = models.TextField(max_length=5000, blank=False, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
