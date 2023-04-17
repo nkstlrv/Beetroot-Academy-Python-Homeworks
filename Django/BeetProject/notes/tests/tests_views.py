5d63f5b1046c5d2070cb4ae3482f2263bda76588
Nikita Stoliarov <stlrvnk@gmail.com>
2023-04-17 18:25:27 +0300

    no pk views tested

--- Django/BeetProject/notes/tests/tests_views.py
+++ Django/BeetProject/notes/tests/tests_views.py
@@ -1,6 +1,7 @@
 from django import urls
 from django.contrib.auth import get_user_model
 import pytest
+from django.urls import reverse
 
 
 @pytest.mark.parametrize('param', [
@@ -15,5 +16,6 @@ def test_render_views(client, param):
     temp_url = urls.reverse(param)
     resp = client.get(temp_url)
     assert resp.status_code == 200
+
 
 
